5fe0c9d5da8adf714472d605ea9d6e5d708127ce
bensonk <bensonk42@gmail.com>
2012-09-10 17:13:03 -0700

    Fleshes out the public facing site a bit

--- panic-webapp/config/routes.rb
+++ panic-webapp/config/routes.rb
@@ -1,5 +1,7 @@
 PanicWebapp::Application.routes.draw do
   resources :leaks
   resources :credentials
+  match '/about' => 'about#about'
+  match '/contact' => 'about#contact'
   root :to => 'about#index'
 end
