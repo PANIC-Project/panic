6bce17bf05dc9bc3c634f76fecadf324353d1945
bensonk <bensonk42@gmail.com>
2012-09-13 18:06:43 -0700

    Adds lots of fun devise goodies

--- panic-webapp/config/routes.rb
+++ panic-webapp/config/routes.rb
@@ -1,5 +1,13 @@
 PanicWebapp::Application.routes.draw do
-  devise_for :users
+  # Set up devise, but skip registration routes
+  #devise_for :users
+  devise_for :users, :skip => [:registrations]
+  # Add in registration edit routes, leaving off new user sign up
+  as :user do
+    get '/users' => 'devise/registrations#create', :as => 'registration'
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
 
   resources :leaks
   resources :credentials
