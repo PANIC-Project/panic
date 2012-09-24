7e8afdf005b6644825b2e821700dde39fb9ea4a8
bensonk <bensonk42@gmail.com>
2012-09-24 10:42:21 +0200

    Makes graphs actual histograms, and not suck

--- panic-webapp/app/assets/javascripts/leaks.js
+++ panic-webapp/app/assets/javascripts/leaks.js
@@ -1,4 +1,20 @@
 function flotable(data) {
-    return _.map(stats.strength.buckets, function(a,b) { return [b, a] });
+    return _.map(data, function(a,b) { return [b, a]; });
 }
 
+function find_width(data) {
+    var ks = Object.keys(data);
+    return parseFloat(ks[1]) - parseFloat(ks[2]);
+}
+
+function make_distribution(canvas, data, color) {
+    if(!color) color = "#FF7";
+    $.plot($(canvas), [ {
+        data: flotable(data.buckets),
+        "color": color,
+        bars: {
+            show: true,
+            barWidth: find_width(data.buckets)
+        }
+    } ]);
+}
