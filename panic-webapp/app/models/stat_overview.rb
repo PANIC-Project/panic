442ec217af326e164c8c24a99c573a39316b047b
bensonk <bensonk42@gmail.com>
2012-09-26 08:25:42 +0200

    Adds scatterplot data

--- panic-webapp/app/models/stat_overview.rb
+++ panic-webapp/app/models/stat_overview.rb
@@ -12,6 +12,7 @@ class StatOverview < ActiveRecord::Base
   def calc_top_passwords
     raw_sql = "SELECT COUNT(password) as pw_count, password FROM credentials WHERE password is not null GROUP BY password ORDER BY pw_count DESC LIMIT 20"
     self.most_common_passwords = ActiveRecord::Base.connection.select_all raw_sql
+    self
   end
 
   def calc_distributions
@@ -19,18 +20,24 @@ class StatOverview < ActiveRecord::Base
     pws = ActiveRecord::Base.connection.select_all(raw_sql)
     pws.collect! { |pw| pw["password"] }
 
+    self.strength_distribution = pws.collect { |pw| pw.strength }.summary
     self.length_distribution = pws.collect { |pw| pw.length }.summary
     self.complexity_distribution = pws.collect { |pw| pw.character_complexity }.summary
-    self.strength_distribution = pws.collect { |pw| pw.strength }.summary
+    self
   end
 
   def calc_scatterplots
-    # TODO
+    leaks = Leak.where("leaked_on IS NOT NULL AND stats IS NOT NULL").order("leaked_on").select { |l| l.stats and l.stats[:length][:median] }
+    self.strength_points = leaks.collect { |l| [ l.leaked_on.to_i * 1000, l.stats[:strength][:median] ] }
+    self.length_points = leaks.collect { |l| [ l.leaked_on.to_i * 1000, l.stats[:length][:median] ] }
+    self.complexity_points = leaks.collect { |l| [ l.leaked_on.to_i * 1000, l.stats[:character_complexity][:median] ] }
+    self
   end
 
   def calc_all
     calc_top_passwords
     calc_distributions
     calc_scatterplots
+    self
   end
 end
